f36461220db570f656250736fa839f3caeca00e6
nick-nugat <onestoptube@gmail.com>
2024-02-18 01:07:40 -0500

    changed page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "👨‍💻 coding away...",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
